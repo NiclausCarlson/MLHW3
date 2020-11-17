bb62f98f717b31ca8ee9ff76d04f7d2c5ba9c283
Nik Carlson <gleb_fedorov_00@mail.ru>
2020-11-17 14:43:26 +0300

    Maybe done

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import numpy as numpy
 import matplotlib.pyplot as plt
 import Utils as Utils
 
-eps = 0.0000000001
+eps = 10e-11
 
 polynomialDegrees = [2, 3, 4, 5]
 sectionForGaussian = [1, 5]
 C = [0.05, 0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0]
 p = 2  # coef to polynomial kernel
 b = 1  # coef to gaussian kernel
-freeCoef = 0
 
 computedKernel = [[]]
 
@@ -21,20 +20,24 @@ listOfKernelResults = [{"linear": 200.0, "polynomial": 200.0, "gaussian": 200.0}
                        {"linear": 200.0, "polynomial": 200.0, "gaussian": 200.0}]
 listOfBestKernelParams = [{"linear": 0, "polynomial": 200.0, "gaussian": 200.0},
                           {"linear": 0, "polynomial": 200.0, "gaussian": 200.0}]
-listOfBestC = [1000, 1000]
+listOfBestC = [{"linear": 0, "polynomial": 200.0, "gaussian": 200.0},
+               {"linear": 0, "polynomial": 200.0, "gaussian": 200.0}]
+
+listOfBestFreeCoefs = [{"linear": 0, "polynomial": 200.0, "gaussian": 200.0},
+                       {"linear": 0, "polynomial": 200.0, "gaussian": 200.0}]
 
 
 def computeKernel(name, x1, x2):
     if name == "linear":
-        return numpy.dot(x1, x2)
+        return sum([a[0] * a[1] for a in zip(x1, x2)])
     elif name == "polynomial":
-        return (1 + numpy.dot(x1, x2)) ** p
+        return (1 + sum([a[0] * a[1] for a in zip(x1, x2)])) ** p
     elif name == "gaussian":
-        return numpy.exp(numpy.multiply(-b, numpy.power(numpy.linalg.norm(numpy.array(x1) - numpy.array(x2)), 2)))
+        return numpy.exp(numpy.multiply(-b, sum([(a[0] - a[1]) ** 2 for a in zip(x1, x2)])))
     return 0
 
 
-def precalcKernel(name, features):
+def predictKernel(name, features):
     global computedKernel
     for i in range(len(features)):
         for j in range(len(features)):
@@ -49,10 +52,10 @@ def getClass(x):
 
 def computeE(coefs, i, classes):
     return sum([getClass(classes[j]) * coefs[j] * computedKernel[i][j] for j in
-                range(0, len(classes))]) - getClass(classes[i])
+                range(0, len(classes))])
 
 
-def computeError(coefs, classes):
+def computeError(coefs, classes, freeCoef):
     wrong = 0
     for i in range(len(classes)):
         predicted = numpy.sign(computeE(coefs, i, classes) + freeCoef)
@@ -62,11 +65,11 @@ def computeError(coefs, classes):
 
 
 def computeSVM(coefs, kernel, c, classes):
-    global freeCoef, computedKernel
+    global computedKernel
     datasetLen = len(classes)
     tmpCoefs = [0 for _ in range(datasetLen)]
+    shuffledArray = [i for i in range(0, datasetLen)]
     for _ in range(1000):
-        shuffledArray = [i for i in range(0, datasetLen)]
         random.shuffle(shuffledArray)
         for step in range(0, datasetLen):
             i = shuffledArray[step]
@@ -81,11 +84,12 @@ def computeSVM(coefs, kernel, c, classes):
                 L = max(0, tmpCoefs[j] + tmpCoefs[i] - c)
                 H = min(c, tmpCoefs[j] + tmpCoefs[i])
 
-            if abs(L - H) >= eps:
+            if abs(L - H) > eps:
                 eta = computedKernel[i][i] + computedKernel[j][j] - 2 * computedKernel[i][j]
                 if eta > eps:
-                    newCoef = tmpCoefs[j] + getClass(classes[j]) * \
-                              (computeE(tmpCoefs, i, classes) - computeE(tmpCoefs, j, classes)) / eta
+                    E1 = computeE(tmpCoefs, i, classes) - getClass(classes[i])
+                    E2 = computeE(tmpCoefs, j, classes) - getClass(classes[j])
+                    newCoef = tmpCoefs[j] + getClass(classes[j]) * (E1 - E2) / eta
                     newCoef = min(H, max(L, newCoef))
                     if abs(newCoef - tmpCoefs[j]) < eps:
                         continue
@@ -99,23 +103,23 @@ def computeSVM(coefs, kernel, c, classes):
         res = 0
         cnt = 0
         for i in range(datasetLen):
-            for j in range(datasetLen):
-                res -= tmpCoefs[j] * getClass(classes[j]) * computedKernel[i][j]
-            res += getClass(classes[i])
-            cnt += 1
-
+            if tmpCoefs[i] >= eps:
+                for j in range(datasetLen):
+                    res -= tmpCoefs[j] * getClass(classes[j]) * computedKernel[i][j]
+                res += getClass(classes[i])
+                cnt += 1
         if cnt == 0:
-            freeCoef = 0
+            tmpFreeCoef = 0
         else:
-            freeCoef = res / cnt
+            tmpFreeCoef = res / cnt
     else:
         res = 0
         for i in range(datasetLen):
             res -= tmpCoefs[i] * getClass(classes[i]) * computedKernel[pos][i]
         res += getClass(classes[pos])
-        freeCoef = res
+        tmpFreeCoef = res
 
-    err = computeError(tmpCoefs, classes)
+    err = computeError(tmpCoefs, classes, tmpFreeCoef)
     if err < listOfKernelResults[currentDatasetNumber][kernel]:
         listOfKernelResults[currentDatasetNumber][kernel] = err
         param = -1
@@ -124,13 +128,14 @@ def computeSVM(coefs, kernel, c, classes):
         elif kernel == "gaussian":
             param = b
         listOfBestKernelParams[currentDatasetNumber][kernel] = param
-        listOfBestC[currentDatasetNumber] = c
+        listOfBestC[currentDatasetNumber][kernel] = c
+        listOfBestFreeCoefs[currentDatasetNumber][kernel] = tmpFreeCoef
         coefs[:] = tmpCoefs.copy()
 
 
 def drawGraph(coefs, kernel, features, classes):
     size = 30
-
+    backgroundSize = size * 10
     backgroundPX = []
     backgroundPY = []
     backgroundNX = []
@@ -142,11 +147,11 @@ def drawGraph(coefs, kernel, features, classes):
     while y < yCond:
         x = -1.5 if currentDatasetNumber == 0 else 0
         while x < xCond:
-            res = freeCoef
+            res = listOfBestFreeCoefs[currentDatasetNumber][kernel]
             for i in range(len(classes)):
                 res += getClass(classes[i]) * coefs[i] * computeKernel(kernel, [x, y], features[i])
 
-            if res >= eps:
+            if res > eps:
                 backgroundPX.append(x)
                 backgroundPY.append(y)
             elif res < -eps:
@@ -155,11 +160,12 @@ def drawGraph(coefs, kernel, features, classes):
             x += step
         y += step
 
+    print(len(backgroundPX), len(backgroundPY), len(backgroundNX), len(backgroundNY))
     fig, ax = plt.subplots()
 
-    ax.scatter(backgroundPX, backgroundPY, marker='s', c=[[0 / 255.0, 75 / 255.0, 75 / 255.0]], s=size * 5,
+    ax.scatter(backgroundPX, backgroundPY, marker='s', c=[[0 / 255.0, 75 / 255.0, 75 / 255.0]], s=backgroundSize,
                label="P class area")
-    ax.scatter(backgroundNX, backgroundNY, marker='s', c=[[1, 1, 0]], s=size * 10, label="N class area")
+    ax.scatter(backgroundNX, backgroundNY, marker='s', c=[[1, 1, 0]], s=backgroundSize, label="N class area")
 
     firstClassX = [features[i][0] for i in range(len(classes)) if getClass(classes[i]) == 1]
     firstClassY = [features[i][1] for i in range(len(classes)) if getClass(classes[i]) == 1]
@@ -185,7 +191,6 @@ def solver(path):
     [features, classes] = Utils.getDataAndClasses(path)
 
     computedKernel = [[0] * len(classes) for _ in range(len(classes))]
-
     features = features.values.tolist()
     classes = classes.values.tolist()
     forLinear = []  # коэфициеты для линейного ядра
@@ -195,31 +200,37 @@ def solver(path):
     for c in C:
         for kernel in kernels:
             if kernel == "linear":
-                precalcKernel(kernel, features)
+                predictKernel(kernel, features)
                 computeSVM(forLinear, kernel, c, classes)
             elif kernel == "polynomial":
                 for i in polynomialDegrees:
                     p = i
-                    precalcKernel(kernel, features)
+                    predictKernel(kernel, features)
                     computeSVM(forPolynomial, kernel, c, classes)
             elif kernel == "gaussian":
                 step = 0.1
                 while b <= sectionForGaussian[1]:
-                    precalcKernel(kernel, features)
+                    predictKernel(kernel, features)
                     computeSVM(forGaussian, kernel, c, classes)
                     b += step
+                b = sectionForGaussian[0]
 
+    print("Done")
     f = open(listOfNames[currentDatasetNumber] + ".txt", "w")
     f.write("--" + listOfNames[currentDatasetNumber] + "--\n")
     f.write("Coefs:\n")
     f.write(str(forLinear) + "\n")
     f.write(str(forPolynomial) + "\n")
     f.write(str(forGaussian) + "\n")
+    f.write("Best free coefs: \n")
+    Utils.printDictionary(listOfBestFreeCoefs, currentDatasetNumber, f)
     f.write("Kernel errors:\n")
     Utils.printDictionary(listOfKernelResults, currentDatasetNumber, f)
     f.write("Kernels param:\n")
     Utils.printDictionary(listOfBestKernelParams, currentDatasetNumber, f)
-    f.write("Best C param: " + str(listOfBestC[currentDatasetNumber]) + "\n")
+    f.write("Best C param:\n")
+    Utils.printDictionary(listOfBestC, currentDatasetNumber, f)
+
     f.close()
     drawGraph(forLinear, "linear", features, classes)
     drawGraph(forPolynomial, "polynomial", features, classes)
@@ -227,10 +238,9 @@ def solver(path):
 
 
 def reeinitParameters():
-    global p, b, freeCoef, computedKernel
+    global p, b, computedKernel
     p = 2
     b = 1
-    freeCoef = 0
     computedKernel.clear()
 
 
